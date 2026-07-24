9de7fd20815344c9b641dca60d99f736612e3c50
mustardcoma6 <kingputo99@gmail.com>
2026-07-25 00:11:23 +0200

    force https

--- fanta-dynasty/app/Providers/AppServiceProvider.php
+++ fanta-dynasty/app/Providers/AppServiceProvider.php
@@ -4,22 +4,19 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        URL::forceScheme('https');
+
         Vite::prefetch(concurrency: 3);
     }
 }
