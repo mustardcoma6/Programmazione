b55e77f75bec49ddba2be5eadb3eafcad56409db
mustardcoma6 <kingputo99@gmail.com>
2026-08-19 18:30:54 +0200

    finanze adj

--- fanta-dynasty/app/Models/MarketValueHistory.php
+++ fanta-dynasty/app/Models/MarketValueHistory.php
@@ -3,5 +3,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class MarketValueHistory extends Model {
+    // Specifichiamo la tabella per evitare confusioni
+    protected $table = 'market_value_histories'; 
     protected $fillable = ['league_id', 'user_id', 'value', 'recorded_at'];
 }
