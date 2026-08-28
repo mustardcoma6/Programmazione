06b8b0ed29ea7940412377311b288fad45a2c970
mustardcoma6 <kingputo99@gmail.com>
2026-08-28 11:19:04 +0200

    fix

--- fanta-dynasty/database/migrations/2026_08_28_105033_add_league_points_to_league_participants.php
+++ fanta-dynasty/database/migrations/2026_08_28_105033_add_league_points_to_league_participants.php
@@ -4,17 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-public function up(): void
+return new class extends Migration
 {
-    Schema::table('league_participants', function (Blueprint $table) {
-        // Aggiungiamo i punti classifica (es: 3, 6, 9...)
-        $table->integer('league_points')->default(0)->after('user_id');
-    });
-}
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('league_participants', function (Blueprint $table) {
+            // Aggiungiamo i punti classifica (es: 3, 1, 0) dopo l'ID utente
+            $table->integer('league_points')->default(0)->after('user_id');
+        });
+    }
 
-public function down(): void
-{
-    Schema::table('league_participants', function (Blueprint $table) {
-        $table->dropColumn('league_points');
-    });
-}
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('league_participants', function (Blueprint $table) {
+            $table->dropColumn('league_points');
+        });
+    }
+};
